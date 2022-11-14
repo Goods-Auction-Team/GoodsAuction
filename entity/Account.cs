5908196d19c179029a868a894232ca2165911fd3
jadshouse <98120629+jadshouse@users.noreply.github.com>
2022-11-13 22:01:59 -0500

    account and item objects started

---
+++ entity/Account.cs
@@ -0,0 +1,17 @@
+using System;
+public class Account
+{
+   private string username;
+   private string password;
+   private string lastLogout;
+
+   public string getUsername();
+   {
+        return username;
+   }
+
+   public string getPassword();
+   {
+        return password;
+   }
+}
---
+++ entity/Item.cs
@@ -0,0 +1,70 @@
+using System;
+public class item 
+{
+    private int itemID;
+    private string itemName;
+    private string itemDescription;
+    private float startingBid;
+    private float currentBid;
+    private DateTime timeRemaining; //not sure if this is correct
+    private string currentLeader;
+    private Account username;
+
+    public int getItemID()
+    {
+        return itemID;
+    }
+    public void setItemID(int itemID)
+    {
+        this itemID = itemID;
+    }
+    public string getItemName()
+    {
+        return itemName;
+    }
+    public void setItemName(string itemName)
+    {
+        this itemName = itemName;
+    }
+    public string getItemDescription()
+    {
+        return itemDescription;
+    }
+    public void setItemDescription(string itemDescription)
+    {
+        this itemDescription = itemDescription;
+    }
+    public float getStartingBid()
+    {
+        return startingBid;
+    }
+    public void setStartingBid(float startingBid)
+    {
+        this startingBid = startingBid;
+    }
+    public float getCurrentBid()
+    {
+        return currentBid;
+    }
+    public void setCurrentBid(float currentBid)
+    {
+        this currentBid = currentBid;
+    }
+    public DateTime getTimeRemaining()
+    {
+        return timeRemaining; //Not sure if this is correct
+    }
+    public void setTimeRemaining(DateTime timeRemaining)
+    {
+        this timeRemaining = timeRemaining;
+    }
+    public Account string getCurrentLeader();
+    {
+        return username; //maybe??
+    }
+    public void setCurrentLeader(string username)
+    {
+        this username = username;
+    }
+
+}
